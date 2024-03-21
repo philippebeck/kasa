0948e52248e35f478408c0168d395b5150f77404
Philippe Beck <philippe@philippebeck.net>
2024-03-21 19:59:39 +0400

    Create the router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+//import Housing from './pages/housing/housing';
+//import About from './pages/about/about';
+//import Error from './pages/error/error';
+import Nav from './components/nav/nav';
+import Footer from './components/footer/footer';
+import logo from './assets/nav.webp';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Nav src={logo} alt="Logo Kasa" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/*<Route path="/housing/:id" element={<Housing />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<Error />} />*/}
+      </Routes>
+      <Footer brand="Kasa" />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
