3f35f56722363e116479d8ac88e71a1de963616a
Philippe Beck <philippe@philippebeck.net>
2024-03-22 20:21:50 +0400

    Active about to index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 //import Housing from './pages/housing/housing';
-//import About from './pages/about/about';
+import About from './pages/about/about';
 //import Error from './pages/error/error';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
@@ -18,9 +18,9 @@ root.render(
       <Nav src={logo} alt="Logo Kasa" />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/*<Route path="/housing/:id" element={<Housing />} />
+        {/*<Route path="/housing/:id" element={<Housing />} />*/}
         <Route path="/About" element={<About />} />
-        <Route path="*" element={<Error />} />*/}
+        {/*<Route path="*" element={<Error />} />*/}
       </Routes>
       <Footer brand="Kasa" />
     </BrowserRouter>
