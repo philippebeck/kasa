88556572f9d7bd65ad7e5df20b7d273213fb185e
Philippe Beck <philippe@philippebeck.net>
2024-03-22 19:06:26 +0400

    Improve footer for jsdoc & class

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,18 @@
 import logo from "../../assets/footer.webp";
 import "./footer.css";
 
+/**
+ * ? FOOTER
+ * * Renders the footer component with the provided brand name.
+ *
+ * @param {Object} props - The props object containing the brand name.
+ * @param {string} props.brand - The name of the brand.
+ * @return {JSX.Element} The rendered footer component.
+ */
 const Footer = ({ brand }) => {
 
   return (
-    <footer>
+    <footer className="footer">
       <img src={logo} alt="Logo" />
 
       <p>
