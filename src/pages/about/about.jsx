5590607a6b8b823785c1751efcc752cd105d70ee
Philippe Beck <philippe@philippebeck.net>
2024-03-22 20:21:19 +0400

    Create about page

---
+++ src/pages/about/about.jsx
@@ -0,0 +1,20 @@
+import about from "../../assets/about.json";
+import img from "../../assets/about.webp";
+import Banner from "../../components/banner/banner";
+import Collapse from "../../components/collapse/collapse";
+import "./about.css";
+
+const About = () => {
+
+  return (
+    <main>
+      <Banner src={img} alt="Des forêts entourées de montagnes" />
+
+      { about.map(({ title, content }) =>
+        <Collapse key={title} title={title} content={content} />
+      )}
+    </main>
+  )
+}
+
+export default About
