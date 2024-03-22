328a8bf1fe5c69a52bbca20c51c84d5c3e5c4f1a
Philippe Beck <philippe@philippebeck.net>
2024-03-22 19:06:59 +0400

    Improve banner for style, jsdoc & classes

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,10 +1,21 @@
 import "./banner.css";
 
-const Banner = ({ src, alt, title="" }) => {
+/**
+ * ? BANNER
+ * * Renders a banner component with an image & a title.
+ *
+ * @param {Object} props - The properties for the banner component.
+ * @param {string} props.src - The source URL of the image.
+ * @param {string} props.alt - The alternative text for the image.
+ * @param {string} [props.title=""] - The title to be displayed.
+ * @param {string} [props.className=""] - Additional CSS class names for the component.
+ * @returns {JSX.Element} The rendered banner component.
+ */
+const Banner = ({ src, alt, title="", className="" }) => {
 
   return (
-    <header>
-      <img src={src} alt={alt} />
+    <header className="banner">
+      <img src={src} alt={alt} className={className} />
 
       <h1>{title}</h1>
     </header>
