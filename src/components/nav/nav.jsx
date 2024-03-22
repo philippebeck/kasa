543f9910bce056653eefc43a7b1173eb20c1cf87
Philippe Beck <philippe@philippebeck.net>
2024-03-22 19:05:59 +0400

    Improve nav for link, jsdoc & class

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 import nav from "../../assets/nav.json";
 import "./nav.css";
 
+/**
+ * ? NAV
+ * * Renders a navigation component with a logo & a list of links.
+ *
+ * @param {Object} props - The props object containing the `src` & `alt` properties.
+ * @param {string} props.src - The source URL for the logo image.
+ * @param {string} props.alt - The alternate text for the logo image.
+ * @return {JSX.Element} The rendered navigation component.
+ */
 const Nav = ({ src, alt }) => {
 
   return (
-    <nav>
-      <Link href="/">
+    <nav className="nav">
+      <Link to="/">
         <img src={src} alt={alt} />
       </Link>
 
@@ -14,7 +23,7 @@ const Nav = ({ src, alt }) => {
         { nav.map(({ link, content }) =>
 
         <li key={link}>
-          <Link href={link}>{content}</Link>
+          <Link to={link}>{content}</Link>
         </li>
 
         )}
