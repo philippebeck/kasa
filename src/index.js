eabca65fb9f163298d3a6d22c5365bb002708186
Philippe Beck <philippe@philippebeck.net>
2024-03-23 12:09:09 +0400

    Active last routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
-//import Housing from './pages/housing/housing';
+import Housing from './pages/housing/housing';
 import About from './pages/about/about';
-//import Error from './pages/error/error';
+import Error from './pages/error/error';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
 import logo from './assets/nav.webp';
@@ -18,9 +18,9 @@ root.render(
       <Nav src={logo} alt="Logo Kasa" />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/*<Route path="/housing/:id" element={<Housing />} />*/}
+        <Route path="/housing/:id" element={<Housing />} />
         <Route path="/About" element={<About />} />
-        {/*<Route path="*" element={<Error />} />*/}
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer brand="Kasa" />
     </BrowserRouter>
