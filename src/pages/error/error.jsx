7c7fb095005afc9bfe908a732d3c5ded811dd4c4
Philippe Beck <philippe@philippebeck.net>
2024-03-23 12:09:24 +0400

    Create error page

---
+++ src/pages/error/error.jsx
@@ -0,0 +1,15 @@
+import "./error.css";
+
+const Error = () => {
+
+  return (
+    <main id="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+
+      <a href="/">Retourner sur la page d’accueil</a>
+    </main>
+  );
+}
+
+export default Error
