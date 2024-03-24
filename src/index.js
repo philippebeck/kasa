040cb2ec991cff79a44f7cab7ce3c551468587c3
Philippe Beck <philippe@philippebeck.net>
2024-03-24 14:00:26 +0400

    Simplify nav & footer call from index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React    from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import logo     from './assets/nav.webp';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home     from './pages/home/home';
 import Housing  from './pages/housing/housing';
 import About    from './pages/about/about';
 import Error    from './pages/error/error';
+
 import Nav      from './components/nav/nav';
 import Footer   from './components/footer/footer';
 
@@ -19,7 +18,7 @@ root.render(
 
   <React.StrictMode>
     <BrowserRouter>
-      <Nav src={logo} alt="Logo Kasa" />
+      <Nav />
 
       <Routes>
         <Route path="/"            element={<Home />} />
@@ -28,7 +27,7 @@ root.render(
         <Route path="*"            element={<Error />} />
       </Routes>
 
-      <Footer brand="Kasa" />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
